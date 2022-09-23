f0e328d65d3ad0e2cad9bd31dcf52e71757860fb
faizanje <malickfaizann@gmail.com>
2022-09-23 17:23:01 +0500

    Success Dialog, orderview added

--- app/src/main/java/com/hfdevs/kav/ui/fragment/orderview/OrderViewFragment.kt
+++ app/src/main/java/com/hfdevs/kav/ui/fragment/orderview/OrderViewFragment.kt
@@ -5,10 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hfdevs.kav.R
+import com.hfdevs.kav.data.model.PreviousNewOrderItemData
 import com.hfdevs.kav.databinding.FragmentOrderViewBinding
+import com.hfdevs.kav.databinding.FragmentPreviousOrderBinding
+import com.hfdevs.kav.ui.fragment.previousorder.PreviousOrderAdapter
+import com.hfdevs.kav.ui.fragment.previousorder.PreviousOrderViewModel
 
 class OrderViewFragment : Fragment() {
+
+    private val viewModel by viewModels<PreviousOrderViewModel>()
+    private lateinit var adapter: PreviousOrderAdapter
+    val list = mutableListOf<PreviousNewOrderItemData>()
+
     lateinit var binding: FragmentOrderViewBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +28,30 @@ class OrderViewFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentOrderViewBinding.inflate(layoutInflater)
+
+        init()
+        observeData()
+        viewModel.fetchData()
+        binding.btnBack.setOnClickListener{view->
+            Navigation.findNavController(view).popBackStack()
+        }
+
         return binding.root
+    }
+
+    private fun observeData() {
+        viewModel.data.observe(viewLifecycleOwner) {
+            list.clear()
+            list.addAll(it)
+            adapter.notifyDataSetChanged()
+
+        }
+    }
+
+    private fun init() {
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        adapter = PreviousOrderAdapter(list,viewModel)
+        binding.recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/hfdevs/kav/ui/fragment/orderview/OrderViewViewModel.kt
@@ -0,0 +1,38 @@
+package com.hfdevs.kav.ui.fragment.orderview
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.hfdevs.kav.R
+import com.hfdevs.kav.data.model.PreviousNewOrderItemData
+
+class OrderViewViewModel(private val app: Application) : AndroidViewModel(app) {
+    val data = MutableLiveData<List<PreviousNewOrderItemData>>()
+
+    val checkList: BooleanArray = booleanArrayOf(false,false,false,false,false,false,false,false,false)
+
+    fun isTrue(pos:Int){
+        checkList[pos]=true
+    }
+    fun isFalse(pos:Int){
+        checkList[pos]=false
+    }
+
+
+
+    fun fetchData() {
+        data.value = listOf(
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+            PreviousNewOrderItemData(app.getString(R.string.tab_text_date)),
+
+
+            )
+    }
+}
